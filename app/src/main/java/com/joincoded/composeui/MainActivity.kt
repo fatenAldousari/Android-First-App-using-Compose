8b79e3020fa3cca80eaf043540b31e0793f6f910
Faten F M F S Aldousari <83330@kfh.com>
2024-01-22 15:49:55 +0300

    update

--- app/src/main/java/com/joincoded/composeui/MainActivity.kt
+++ app/src/main/java/com/joincoded/composeui/MainActivity.kt
@@ -1,67 +1,37 @@
 package com.joincoded.composeui
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.joincoded.composeui.ui.theme.ComposeUITheme
+import kotlinx.coroutines.delay
+
+data class Question(val text: String, val isCorrect: Boolean)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUITheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-
-                    Column(
-                        //modifier = Modifier,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
-                    )
-                    {
-                        Question("is faten my name?")
-
-                        Button(onClick = { /*TODO*/ }) {
-                            Text("next question")
-                        }
-
-
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.Bottom,
-
-                        )
-                        {
-
-                            Button(onClick = { /*TODO*/ }) {
-                                Text("true")
-
-                            }
-                            Button(onClick = { /*TODO*/ }) {
-                                Text("false")
-                            }
-                        }
-
-                    }
+                    TrueFalseGame()
                 }
             }
         }
@@ -69,20 +39,144 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Question(question: String, modifier: Modifier = Modifier, fontSize: TextUnit = 25.sp) {
-    Text(
-        text = "$question!",
-        modifier = modifier,
-        fontSize = fontSize,
-        fontWeight = FontWeight.Bold
-    )
+fun TrueFalseGame() {
+    var showNextButton by remember { mutableStateOf(false) }
+    var questions by remember { mutableStateOf<List<Question>>(emptyList()) }
+    var currentQuestionIndex = remember { mutableStateOf(0) }
+    var AnsrCorrect by remember { mutableStateOf(false) }
+    var UserAnsr by remember { mutableStateOf(false) }
+    var ButtonAnsr by remember { mutableStateOf(false) }
+    var showFeedback by remember { mutableStateOf(false) }
+    var showNextQuestion by remember { mutableStateOf(false) }
+    var userScore = remember { mutableStateOf(0) }
+
+    if (questions.isEmpty()) {
+        questions = listOf(
+            Question("is faten my name? ", true),
+            Question("my fav move is harry potter", false),
+            Question("i love cats", true),
+        )
+    }
+    AnsrCorrect = UserAnsr == questions[currentQuestionIndex.value].isCorrect
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = questions[currentQuestionIndex.value].text,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+        var showResetButton = currentQuestionIndex.value == questions.size - 1
+        if (showResetButton) {
+            ResetButton(currentQuestionIndex, userScore)
+        }
+
+        if (AnsrCorrect && showFeedback && !showResetButton) {
+            AnswerFeedback("Correct!", MaterialTheme.colorScheme.secondary)
+            Text(text = "Score: ${userScore.value}")
+        }
+
+        if (showNextButton) {
+            Button(
+                onClick = {
+                    ButtonAnsr = false
+                    showFeedback = false
+                    showNextButton = false
+                    if (currentQuestionIndex.value < questions.size - 1) {
+                        currentQuestionIndex.value++
+                    }
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(10.dp),
+            ) {
+                Text(text = "Next Question")
+            }
+        }
+
+        if (!showNextButton && !AnsrCorrect && showFeedback) {
+            ButtonAnsr = true
+            AnswerFeedback("Wrong!", MaterialTheme.colorScheme.error)
+        }
+
+        if (!showNextButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TrueFalseButton("True") {
+                    UserAnsr = true
+                    ButtonAnsr = true
+                    AnsrCorrect = UserAnsr == questions[currentQuestionIndex.value].isCorrect
+                    userScore.value += 1
+                    showFeedback = true
+                    showNextButton = true
+                }
+                TrueFalseButton("False") {
+                    UserAnsr = false
+                    ButtonAnsr = true
+                    AnsrCorrect = UserAnsr == questions[currentQuestionIndex.value].isCorrect
+                    showFeedback = true
+                    showNextButton = true
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ResetButton(currentQuestionIndex: MutableState<Int>, userScore: MutableState<Int>) {
+    Button(onClick = {
+        currentQuestionIndex.value = 0
+        userScore.value = 0
+    }) {
+        Text(text = "Restart")
+    }
+}
+
+@Composable
+fun TrueFalseButton(text: String, UserAnsr: () -> Unit) {
+    Button(
+        onClick = {
+            UserAnsr()
+        },
+        modifier = Modifier
+            .width(120.dp)
+            .height(40.dp)
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun AnswerFeedback(message: String, backgroundColor: androidx.compose.ui.graphics.Color) {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .clip(CircleShape)
+            .clip(MaterialTheme.shapes.large)
+            .background(backgroundColor)
+            .padding(16.dp),
+        Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = message,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeUITheme {
-        Question("Android")
-
-    }
+fun TrueFalseGamePreview() {
+    TrueFalseGame()
 }
